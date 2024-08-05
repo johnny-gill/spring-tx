0c4ef424c550a9af6fbbf7cd54f97fc30f52d6af
johnny-gill <uint777@gmail.com>
2024-08-05 17:01:51 +0900

    프록시 내부 호출 - 프록시를 거치지 않는 문제

---
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,67 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.swing.text.html.BlockView;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService.getClass()={}", callService.getClass());
+        // @Transactional이 포함돼서 때문에 프록시 객체 주입
+        // class hello.springtx.apply.InternalCallV1Test$CallService$$SpringCGLIB$$0
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1Config {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+
+        void external() {
+            log.info("call external");
+            printTxInfo();
+            internal();
+        }
+
+        private static void printTxInfo() {
+            boolean isTxActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("isTxActive={}", isTxActive);
+        }
+
+        @Transactional
+        void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+    }
+}
